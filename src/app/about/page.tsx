fb539bf7ae4b0f660f124ce40df02c0149b45db0
Caroline Shows <caroline.e.shows@gmail.com>
2025-01-13 13:31:16 -0700

    final touches added for basic mobile first styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
-            <p>Hi, I'm Caroline. I'm a software engineer and I love to build things.</p>
+        <div className='bg-primary h-screen p-2'>
+            <h1 className='font-playwrite text-xl m-2'>About</h1>
+            <p>coming soon</p>
         </div>
     )
 };
