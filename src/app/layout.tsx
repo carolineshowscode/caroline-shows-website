700190dc9d1a66c316040f9cdb46496e3485303e
Caroline Shows <caroline.e.shows@gmail.com>
2025-01-20 23:37:51 -0700

    missed import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Playwrite_IN, Dosis } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/Nav";
+import Nav from "@/components/nav";
 
 
 const playwrite = Playwrite_IN({
