fa82185ef5eb510fe1c86e8208973678a2069fcf
Caroline Shows <caroline.e.shows@gmail.com>
2025-02-19 12:29:23 -0700

    add event analytics

--- src/components/resumeButton.tsx
+++ src/components/resumeButton.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { sendGAEvent } from '@next/third-parties/google';
+
 export default function ResumeButton() {
 
     const handleClick = () => {
+        sendGAEvent('event', 'select_content', {'app_name': 'Caroline Shows Website', 'content_type': 'Resume'});
         window.open('https://carolines-website.s3.us-east-2.amazonaws.com/Caroline+Eubanks+Shows+Resume.pdf', '_blank')
     }
 
